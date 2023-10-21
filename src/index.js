95152aa36a45bec7acd7355e755c04272fe1ce74
M-Parsaei <khodadostetdarm@gmail.com>
2023-10-21 13:10:28 -0700

    chore:added axios requests

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from "react-dom/client";
 import {BrowserRouter} from "react-router-dom";
 import './index.css';
 import App from './App';
+import { ChakraProvider } from '@chakra-ui/react';
+import axios from "axios";
+
+axios.defaults.baseURL = process.env.REACT_APP_BACKEND;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ChakraProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>
 );
 
