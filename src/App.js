4afd3ea70d992a25f85e8fdd94399ba473512f58
M-Parsaei <khodadostetdarm@gmail.com>
2023-10-21 11:40:07 -0700

    chore: added react routers and login page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 
 function App() {
   return (
-    <div></div>
+    <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+    </Routes>
   );
 }
 
