c7e31d81b43609813741c471ef1d7be057db0441
M-Parsaei <khodadostetdarm@gmail.com>
2023-10-21 13:58:42 -0700

    added request sending for register and login

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.REACT_APP_BACKEND;
+
+export const axiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_BACKEND,
+  withCredentials: true,
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
