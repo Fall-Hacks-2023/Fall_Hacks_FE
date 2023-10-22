0cb305ef38d540aceda67a15aec463c700fe8527
GurkiratSinghArora <sgurkirat868@gmail.com>
2023-10-21 17:06:45 -0700

    chore: Added list of habits

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import { useRecoilValue } from "recoil";
 import { axiosInstance } from "..";
 import Dashboard from "../component/Dashboard";
 import Generic from "../component/Generic";
+import List from "../component/List";
 const Home = () => {
 //const name = useRecoilValue(userNameState);
 const [user,setUser]=useState(null);
@@ -22,40 +23,8 @@ useEffect(()=>{
 },[])
 return (
     <>
-      <Grid
-        templateColumns="repeat(5, 1fr)"
-        gap={6}
-        h="35"
-        w="90%"
-        bg="#581584"
-        borderRadius={20}
-        margin={"auto"}
-        marginTop={10}
-        display={"flex"}
-        flexDirection={"row"}
-      >
-        <GridItem w="90%" h="10" color={"white"} marginLeft={20}>
-          Home
-        </GridItem>
-        <GridItem w="90%" h="10" color={"white"}>
-          About
-        </GridItem>
-        <GridItem w="100%" h="10" color={"white"}>
-          Reminders
-        </GridItem>
-        <GridItem w="90%" h="10" color={"white"}>
-          Rewards
-        </GridItem>
-
-        <GridItem w="90%" h="10" paddingLeft={"40"} color={"white"}>
-          Log In
-        </GridItem>
-        <GridItem w="90%" h="10" color={"white"}>
-          Sign Up
-        </GridItem>
-      </Grid>
-      {user? <Dashboard/>:<Generic/>}
-    </>
+    <List/>
+      </>
   );
 };
 
