ce96fa23091e3c27bb564a38023b9d0bf3a398c6
Salman Ayaz <salmanayazz@gmail.com>
2023-10-21 18:36:48 -0700

    chore: setup habit form

--- src/component/Panel.jsx
+++ src/component/Panel.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import "./components.css";
 import List from "./List";
+import HabitForm from "./HabitForm";
 
 export default function Panel() {
   return (
     <div className="panel-container">
       <div className="stat-container">
         <div className="stat-container-1"></div>
-        <div className="stat-container-2"></div>
+        <div className="stat-container-2">
+          <HabitForm />
+        </div>
       </div>
       <div className="habits-cotainer">
         <List />
