3412a61dadf025e2286d3f1763e97529d26d92b0
M-Parsaei <khodadostetdarm@gmail.com>
2023-10-21 18:40:35 -0700

    chore:added delete option

--- src/component/List.jsx
+++ src/component/List.jsx
@@ -1,32 +1,90 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { axiosInstance } from "..";
-import { AiFillDelete } from 'react-icons/ai';
-import "./components.css"
-import { Progress } from '@chakra-ui/react'
+import { AiFillDelete } from "react-icons/ai";
+import "./components.css";
+import { Center, Flex, Progress } from "@chakra-ui/react";
 
 const List = () => {
-    const [habits,setHabits] = useState(null);
-    const fetchData = async() =>{
-        const {data} = await axiosInstance.get('/habit');
-        console.log("Habits:",data);
-        setHabits(data)
-    }
-    useEffect(() => {
-        fetchData();
-    }, [])
-  return (
-    <div>
-        {habits && habits.map((habit) =>(
-            <div className="habit-row" >
-                <div className='habit-title'>{habit.title}</div>
-                <div className='habit-date-1'>{habit.achievementDate.split("T")[0]}</div>
-                <div className='habit-date-2'>{habit.dateCreated.split("T")[0]}</div>
-                <div className='habit-progress'><Progress hasStripe value={64} /></div>
-                <div className='habit-delete'><AiFillDelete/></div>
-            </div>
-        ))}
-        </div>
-  )
+  const [habits, setHabits] = useState(null);
+  const fetchData = async () => {
+    const { data } = await axiosInstance.get("/habit");
+    console.log("Habits:", data);
+    setHabits(data);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-}
+  const deleteHabit = async (id) => {
+    try {
+      const { data } = await axiosInstance.delete(`/habit/${id}`);
+      console.log(id);
+      fetchData();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <>
+      {habits  && habits.length > 0 ? (
+        <div className="habit-headers">
+          <div>
+            <span>Title</span>
+          </div>
+          <div>
+            <span>Created At</span>
+          </div>
+          <div>
+            <span>Due by</span>
+          </div>
+          <div>
+            <span>Time spent</span>
+          </div>
+          <div id="remove-header">
+            <span>Remove</span>
+          </div>
+        </div>
+      ) : (
+        <div className="no-habit-found">No Habits Found, add one !</div>
+      )}
+      <div>
+        {habits &&
+          habits.map((habit) => {
+            let dateDiff = new Date(habit.achievementDate) - new Date(habit.dateCreated);
+            let progressPercentage = 100 - (
+              ((new Date(habit.achievementDate) - Date.now()) / dateDiff) *
+              100
+            ).toFixed(2);
+            progressPercentage = Math.floor(progressPercentage);
+            console.log(dateDiff);
+            console.log("hhh");
+            console.log(progressPercentage);
+            return (
+              <div className="habit-row">
+                <div className="habit-title">{habit.title}</div>
+                <div className="habit-date-1">
+                  {habit.achievementDate.split("T")[0]}
+                </div>
+                <div className="habit-date-2">
+                  {habit.dateCreated.split("T")[0]}
+                </div>
+                <div className="habit-progress">
+                  <Progress hasStripe value={progressPercentage} />
+                </div>
+                <div
+                  className="habit-delete"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    deleteHabit(habit._id);
+                  }}
+                >
+                  <AiFillDelete />
+                </div>
+              </div>
+            );
+          })}
+      </div>
+    </>
+  );
+};
 export default List;
